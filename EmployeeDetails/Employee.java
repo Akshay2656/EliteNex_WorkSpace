477892b9ed802835142eb2274917d4698d3b3d8a
Akshay2656 <akshay.paste25@gmail.com>
2026-06-16 20:32:24 +0530

    Modified Employee file

--- EmployeeDetails/Employee.java
+++ EmployeeDetails/Employee.java
@@ -11,38 +11,6 @@ class Employee{
 		this.department = department;
 	}
 	
-	// public void setId(int empId){
-		// this.empId = empId;
-	// }
-	
-	// public int getId(){
-		// return empId;
-	// }
-	
-	// public void setName(String empName){
-		// this.empName = empName;
-	// }
-	
-	// public String getName(){
-		// return empName;
-	// }
-	
-	// public void setSalary(double salary){
-		// this.salary = salary;
-	// }
-
-	// public double getSalary(){
-		// return salary;
-	// }	
-	
-	// public void setDepartment(String department){
-		// this.department = department;
-	// }
-	
-	// public String getDepartment(){
-		// return department;
-	// }
-	
 	public String toString(){
 		return "EmployeeId:"+empId+"\nEmployeeName :"+empName+"\nEmployeeSalary: "+salary+"\nEmployee Department: "+department;
 	}
